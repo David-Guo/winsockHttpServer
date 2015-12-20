57ac8167ed5b17ed9ed4e55ff37b1d832dd7dc3e
David Guo <guodw3@mail2.sysu.edu.cn>
2015-12-20 16:25:19 +0800

    加入事件处理代码与相关类

--- NP_hw3.cpp
+++ NP_hw3.cpp
@@ -1,5 +1,8 @@
 #include <windows.h>
 #include <list>
+#include <assert.h>
+#include <vector>
+#include "client.h"
 using namespace std;
 
 #include "resource.h"
@@ -7,6 +10,7 @@ using namespace std;
 #define SERVER_PORT 7799
 
 #define WM_SOCKET_NOTIFY (WM_USER + 1)
+#define WM_SOCKET_NOTIFY_SERVER (WM_USER + 2)
 
 BOOL CALLBACK MainDlgProc(HWND, UINT, WPARAM, LPARAM);
 int EditPrintf (HWND, TCHAR *, ...);
@@ -14,11 +18,21 @@ int EditPrintf (HWND, TCHAR *, ...);
 //	Global Variables
 //=================================================================
 list<SOCKET> Socks;
+clientHandler globalClientHandler;
+
+void decodeEnv(string sourceString, vector<string>& vDest) {
+	assert(sourceString != "");
+	stringstream source(sourceString);
+	string splited;
+	while(getline(source, splited, '&'))
+		vDest.push_back(splited);
+}
+
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
-	LPSTR lpCmdLine, int nCmdShow)
+				   LPSTR lpCmdLine, int nCmdShow)
 {
-	
+
 	return DialogBox(hInstance, MAKEINTRESOURCE(ID_MAIN), NULL, MainDlgProc);
 }
 
@@ -35,94 +49,321 @@ BOOL CALLBACK MainDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 
 	switch(Message) 
 	{
-		case WM_INITDIALOG:
-			hwndEdit = GetDlgItem(hwnd, IDC_RESULT);
+	case WM_INITDIALOG:
+		hwndEdit = GetDlgItem(hwnd, IDC_RESULT);
+		break;
+	case WM_COMMAND:
+		switch(LOWORD(wParam))
+		{
+		case ID_LISTEN:
+
+			WSAStartup(MAKEWORD(2, 0), &wsaData);
+
+			//create master socket
+			msock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+
+			if( msock == INVALID_SOCKET ) {
+				EditPrintf(hwndEdit, TEXT("=== Error: create socket error ===\r\n"));
+				WSACleanup();
+				return TRUE;
+			}
+
+			err = WSAAsyncSelect(msock, hwnd, WM_SOCKET_NOTIFY, FD_ACCEPT | FD_CLOSE | FD_READ | FD_WRITE);
+
+			if ( err == SOCKET_ERROR ) {
+				EditPrintf(hwndEdit, TEXT("=== Error: select error ===\r\n"));
+				closesocket(msock);
+				WSACleanup();
+				return TRUE;
+			}
+
+			//fill the address info about server
+			sa.sin_family		= AF_INET;
+			sa.sin_port			= htons(SERVER_PORT);
+			sa.sin_addr.s_addr	= INADDR_ANY;
+
+			//bind socket
+			err = bind(msock, (LPSOCKADDR)&sa, sizeof(struct sockaddr));
+
+			if( err == SOCKET_ERROR ) {
+				EditPrintf(hwndEdit, TEXT("=== Error: binding error ===\r\n"));
+				WSACleanup();
+				return FALSE;
+			}
+
+			err = listen(msock, 2);
+
+			if( err == SOCKET_ERROR ) {
+				EditPrintf(hwndEdit, TEXT("=== Error: listen error ===\r\n"));
+				WSACleanup();
+				return FALSE;
+			}
+			else {
+				EditPrintf(hwndEdit, TEXT("=== Server START ===\r\n"));
+			}
+
 			break;
-		case WM_COMMAND:
-			switch(LOWORD(wParam))
-			{
-				case ID_LISTEN:
-
-					WSAStartup(MAKEWORD(2, 0), &wsaData);
-
-					//create master socket
-					msock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-
-					if( msock == INVALID_SOCKET ) {
-						EditPrintf(hwndEdit, TEXT("=== Error: create socket error ===\r\n"));
-						WSACleanup();
-						return TRUE;
-					}
-
-					err = WSAAsyncSelect(msock, hwnd, WM_SOCKET_NOTIFY, FD_ACCEPT | FD_CLOSE | FD_READ | FD_WRITE);
-
-					if ( err == SOCKET_ERROR ) {
-						EditPrintf(hwndEdit, TEXT("=== Error: select error ===\r\n"));
-						closesocket(msock);
-						WSACleanup();
-						return TRUE;
-					}
-
-					//fill the address info about server
-					sa.sin_family		= AF_INET;
-					sa.sin_port			= htons(SERVER_PORT);
-					sa.sin_addr.s_addr	= INADDR_ANY;
-
-					//bind socket
-					err = bind(msock, (LPSOCKADDR)&sa, sizeof(struct sockaddr));
-
-					if( err == SOCKET_ERROR ) {
-						EditPrintf(hwndEdit, TEXT("=== Error: binding error ===\r\n"));
-						WSACleanup();
-						return FALSE;
-					}
-
-					err = listen(msock, 2);
-		
-					if( err == SOCKET_ERROR ) {
-						EditPrintf(hwndEdit, TEXT("=== Error: listen error ===\r\n"));
-						WSACleanup();
-						return FALSE;
-					}
-					else {
-						EditPrintf(hwndEdit, TEXT("=== Server START ===\r\n"));
-					}
-
-					break;
-				case ID_EXIT:
-					EndDialog(hwnd, 0);
-					break;
-			};
-			break;
-
-		case WM_CLOSE:
+		case ID_EXIT:
 			EndDialog(hwnd, 0);
 			break;
+		};
+		break;
 
-		case WM_SOCKET_NOTIFY:
-			switch( WSAGETSELECTEVENT(lParam) )
-			{
-				case FD_ACCEPT:
-					ssock = accept(msock, NULL, NULL);
-					Socks.push_back(ssock);
-					EditPrintf(hwndEdit, TEXT("=== Accept one new client(%d), List size:%d ===\r\n"), ssock, Socks.size());
-					break;
-				case FD_READ:
-				//Write your code for read event here.
-					
-					break;
-				case FD_WRITE:
-				//Write your code for write event here
+	case WM_CLOSE:
+		EndDialog(hwnd, 0);
+		break;
 
-					break;
-				case FD_CLOSE:
-					break;
-			};
+	case WM_SOCKET_NOTIFY:
+		switch( WSAGETSELECTEVENT(lParam) )
+		{
+		case FD_ACCEPT:
+			ssock = accept(msock, NULL, NULL);
+			Socks.push_back(ssock);
+			EditPrintf(hwndEdit, TEXT("=== Accept one new client(%d), List size:%d ===\r\n"), ssock, Socks.size());
 			break;
-		
-		default:
-			return FALSE;
+		case FD_READ:
+			//Write your code for read event here.
+			for (std::list<SOCKET>::iterator it = Socks.begin(); it != Socks.end(); it++) {
+				char *requestBuff;
+				requestBuff = new char[1024];
+				int recvStates = recv(*it, requestBuff, 1024*sizeof(char), 0);
+				// 接受消息失败
+				if (recvStates <= 0) {
+					delete[] requestBuff;
+					EditPrintf(hwndEdit, TEXT("recv error"));
+					continue;
+				}
+				// 接受成功
+				EditPrintf(hwndEdit, TEXT("request: "));
+				EditPrintf(hwndEdit, TEXT(requestBuff));
+				EditPrintf(hwndEdit, TEXT("\r\n"));
 
+				// 解析协议 得到querystring 环境变量与执行程序名
+				string recvMsg;
+				recvMsg = string(requestBuff);
+				stringstream ss(recvMsg);
+				string queryString;
+				string requestDoc;
+				string requestLine;
+				string offset;
+
+				// 取出请求行
+				getline(ss, requestLine, '\n');
+				stringstream ssRqstLine(requestLine);
+
+				if (requestLine.find(".cgi") != string::npos) 
+				{
+					delete[] requestBuff;
+					EditPrintf(hwndEdit, TEXT("not request a cgi doc"));
+					continue;
+				}
+
+				getline(ssRqstLine, offset, '/');
+				getline(ssRqstLine, offset, '?');
+				requestDoc = offset;
+				getline(ssRqstLine, offset, ' ');
+				queryString = offset;
+				// 开启终端查看
+				//AllocConsole();
+				//freopen("CONOUT$", "w", stderr);
+				//cerr << "requestDoc: " << requestDoc << endl;
+				//cerr << "queryString: " << queryString << endl;
+
+				// CGI 部分，解析环境变量 queryString
+				vector<string> vDecodeEnv;
+				decodeEnv(queryString, vDecodeEnv);
+
+				bool isInvalid = false;
+				for (int i = 0; i < vDecodeEnv.size(); i += 3) {
+					// 符号 = 后面为空，即为无效的请求
+					if (vDecodeEnv[0].find_first_of('=') == vDecodeEnv[0].size() - 1) {
+						isInvalid = true;
+						break;
+					}
+					else {
+						string serverIP = vDecodeEnv[i].substr(vDecodeEnv[i].find_first_of('=') + 1);
+						string serverPort = vDecodeEnv[i + 1].substr(vDecodeEnv[i + 1].find_first_of('=') + 1);
+						string serverFile = vDecodeEnv[i + 2].substr(vDecodeEnv[i + 2].find_first_of('=') + 1);
+
+						// 初始化 winsock
+						int ithResult;
+						WSADATA wsaData;
+						ithResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+						if (ithResult != NO_ERROR) {
+							EditPrintf(hwndEdit, TEXT("=== win socket start up fail ===\r\n"));
+							return 1;
+						}
+
+						// 创建 socket
+						SOCKET m_socket;
+						m_socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+						if (m_socket == INVALID_SOCKET) {
+							EditPrintf(hwndEdit, TEXT("=== creat socket fail ===\r\n"));
+							WSACleanup();
+							return FALSE;
+						}
+
+						// 设定 ras server 地址端口信息
+						struct hostent *he;
+						he = gethostbyname(serverIP.c_str());
+						if (he == NULL) {
+							EditPrintf(hwndEdit, TEXT("=== error: getting host name ===\r\n"));
+							WSACleanup();
+							return FALSE;
+						}
+						SOCKADDR_IN client_sin;
+						memset(&client_sin, 0, sizeof(client_sin));
+						client_sin.sin_family = AF_INET;
+						client_sin.sin_addr.s_addr = *((unsigned long long *)he->h_addr);
+						client_sin.sin_port = htons(atoi(serverPort.c_str()));
+						EditPrintf(hwndEdit, TEXT("=== finish setting server ===\n"));
+
+						// 注册同步事件（event）
+						err = WSAAsyncSelect(m_socket, hwnd, WM_SOCKET_NOTIFY_SERVER, FD_CLOSE | FD_READ | FD_WRITE);
+						if ( err == SOCKET_ERROR ) {
+							EditPrintf(hwndEdit, TEXT("=== Error: select error ===\r\n"));
+							closesocket(m_socket);
+							WSACleanup();
+							return TRUE;
+						}
+						// 连接到 ras 服务器
+						connect(m_socket, (SOCKADDR *)&client_sin, sizeof(client_sin));
+						EditPrintf(hwndEdit, TEXT("=== connection build with server ===\r\n"));
+
+						// 初始化 client job
+						singleClient *singleClie;
+						singleClie = globalClientHandler.findClient(*it);
+						if (singleClie == NULL); // 没有这个 client
+						else {
+							singleClie->init(*it);
+							Client *rasClient;
+							rasClient = singleClie->getANewJob();
+							rasClient->init(serverIP, serverPort, serverFile, singleClie->currentUserNum);
+							EditPrintf(hwndEdit, TEXT("=== successful init a client ===\r\n"));
+						}
+
+					} // 结束对 vDecodeEnv.size() 的迭代，表示一个请求初始化结束
+
+					if (isInvalid == true) { 
+						// 无效的请求
+						delete[] requestBuff;
+						continue;
+					}
+					singleClient *singleClie;
+					singleClie = globalClientHandler.findClient(*it);
+					// 生成 html 标签
+					string htmlpage;
+					htmlpage = string("<html>")
+						+ string("<head>")
+						+ string("<meta http-equiv=\"Content-Type\" content=\"text/html; charset=big5\" />")
+						+ string("<title>Network Programming Homework 3</title>")
+						+ string("</head>")
+						+ string("<body bgcolor=#336699>")
+						+ string("<font face=\"Courier New\" size=2 color=#FFFF99>")
+						+ string("<table width=\"800\" border=\"1\">")
+						+ string("<tr>");
+					for(int i = 0; i < singleClie->getUserNum(); i++) {
+						Client *rasClient;
+						rasClient = singleClie->getAnExisJob(i);
+						if(rasClient->Port == "") continue;
+						else 
+							htmlpage = string("<td valign=\"top\" id=\"m") + string(to_string((unsigned long long)i)) + string("\"></td>");
+						string webpage2;
+						webpage2 += string("</tr></table>\n");
+						// 向浏览器发送 html css 标签
+						send(singleClie->serverSocketFD, htmlpage.c_str(), htmlpage.size(),0);
+						send(singleClie->serverSocketFD, webpage2.c_str(), webpage2.size(),0);
+					}
+
+					delete[] requestBuff;
+				}	// 结束对 Socks.size() 的迭代
+			}
+
+			break;
+		case FD_WRITE:
+			//Write your code for write event here
+
+			break;
+		case FD_CLOSE:
+			break;
+		};
+		break;  // 结束 WM_SOCKET_NOTIFY: 事件
+
+	case WM_SOCKET_NOTIFY_SERVER:
+		switch( WSAGETSELECTEVENT(lParam)) {
+		case FD_READ:
+			// vClient.size() 表示浏览器请求数量
+			for (int k = 0; k < globalClientHandler.vClients.size(); k++) {
+				singleClient *singleClie;
+				singleClie = &(globalClientHandler.vClients[k]);
+
+				// userNum 表示 ras 数量
+				for (int j = 0; j < singleClie->getUserNum(); j++) {
+					Client *rasClient;
+					rasClient = singleClie->getAnExisJob(j);
+					// 从 ras 接受数据
+					rasClient->reciveFromServ();
+					string htmlpage = rasClient->returnRsltHtml() + string("\n");
+					// 向浏览器发送 从 ras 接受到的数据
+					send(singleClie->serverSocketFD, htmlpage.c_str(), htmlpage.size(), 0);
+
+					if (rasClient->isSend && rasClient->isExit) 
+						continue;
+					// 可以向 ras 发送从文件读取的指令
+					if (rasClient->isSend)
+						rasClient->sendToServ();
+					Sleep(1000);
+					// ????
+					if (rasClient->isExit) {
+						Sleep(3000);
+						// 从 ras 读取最后一次离开信息，连同 exit 指令一起发送给浏览器
+						rasClient->reciveFromServ();
+						htmlpage = rasClient->returnRsltHtml() + string("\n");
+						send(singleClie->serverSocketFD, htmlpage.c_str(), htmlpage.size(), 0);
+						continue;
+					}
+					else {
+						// 向浏览器发送文件指令
+						htmlpage = rasClient->returnRsltHtml() + string("\n");
+						send(singleClie->serverSocketFD, htmlpage.c_str(), htmlpage.size(), 0);
+						rasClient->isSend = FALSE;
+					}
+				}
+
+				// 检查是否结束了输出 HTML 
+				bool isAllExit = true;
+				for (int i = 0; i < singleClie->getUserNum(); i++) {
+					Client *rasClient;
+					rasClient = singleClie->getAnExisJob(i);
+
+					if(((rasClient->isExit == TRUE) && (rasClient->isSend)) == FALSE) {
+						isAllExit = FALSE;
+						break;
+					}
+				}
+				if (isAllExit) {
+					EditPrintf(hwndEdit, TEXT("===  IS READY TO OUTPUT A WEBPAGE ==="));
+					string endHTML = "";
+					endHTML += string("</font>")
+						+ string("</body>")
+						+ string("</html>\n");
+
+					send(singleClie->serverSocketFD, endHTML.c_str(), endHTML.size(), 0);
+					closesocket(singleClie->serverSocketFD);
+					globalClientHandler.deleteClient(singleClie->serverSocketFD);
+					k--;
+				}
+
+			}
+		case FD_WRITE:
+			break;
+		case FD_CLOSE:
+			break;
+		}
+		break; // 结束 WM_SOCKET_NOTIFY_SERVER: 事件
+
+	default:
+		return FALSE;
 
 	};
 
@@ -131,15 +372,15 @@ BOOL CALLBACK MainDlgProc(HWND hwnd, UINT Message, WPARAM wParam, LPARAM lParam)
 
 int EditPrintf (HWND hwndEdit, TCHAR * szFormat, ...)
 {
-     TCHAR   szBuffer [1024] ;
-     va_list pArgList ;
+	TCHAR   szBuffer [2048] ;
+	va_list pArgList ;
 
-     va_start (pArgList, szFormat) ;
-     wvsprintf (szBuffer, szFormat, pArgList) ;
-     va_end (pArgList) ;
+	va_start (pArgList, szFormat) ;
+	wvsprintf (szBuffer, szFormat, pArgList) ;
+	va_end (pArgList) ;
 
-     SendMessage (hwndEdit, EM_SETSEL, (WPARAM) -1, (LPARAM) -1) ;
-     SendMessage (hwndEdit, EM_REPLACESEL, FALSE, (LPARAM) szBuffer) ;
-     SendMessage (hwndEdit, EM_SCROLLCARET, 0, 0) ;
-	 return SendMessage(hwndEdit, EM_GETLINECOUNT, 0, 0); 
+	SendMessage (hwndEdit, EM_SETSEL, (WPARAM) -1, (LPARAM) -1) ;
+	SendMessage (hwndEdit, EM_REPLACESEL, FALSE, (LPARAM) szBuffer) ;
+	SendMessage (hwndEdit, EM_SCROLLCARET, 0, 0) ;
+	return SendMessage(hwndEdit, EM_GETLINECOUNT, 0, 0); 
 }
---
+++ client.h
@@ -0,0 +1,214 @@
+#include <string.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+// 这个类作为 ras 的客户，主要负责 cgi 的事务
+// 1. 保存从 query_string 中解析得到的 ip port batfile
+// 2. 建立与 ras 的连接，作为 ras 的 client
+// 3. 输入数据，接受数据，并用 html 标签封装好输入输出
+// 复用了 part 1 cgi 中 linux 环境下的 client 类
+class Client{
+public:
+	string IP;
+	string Port;
+	string inputFile;
+	string batFile;
+	int taskId;
+	int sockfd;
+	string resultHtml;
+	streampos m_lastFilePosition;
+	bool isSend;
+	bool isExit;
+
+	std::string substitute(std::string str, const std::string& src, const std::string& dest) {
+		size_t start = 0;
+		while ((start = str.find(src, start)) != std::string::npos) {
+			str.replace(start, src.length(), dest);
+			start += dest.length();
+		}
+		return str;
+	}
+	// 给字符串加上 javascript 标签
+	void Client::saveToResult(string addStr) {
+		while(addStr != "" && addStr[addStr.size() - 1] == 13) 
+			addStr.pop_back();
+
+		resultHtml += "<script>document.all['m";
+		resultHtml += to_string(taskId);
+		resultHtml += "'].innerHTML += \"";
+		resultHtml += addStr;
+		resultHtml += "<br>\";</script>\n";
+	}
+
+	void Client::init(string ip, string port, string file, int id) {
+		IP = ip;
+		Port = port;
+		resultHtml = "";
+		taskId = id;
+		inputFile = file;
+		m_lastFilePosition = 0;
+		isSend = FALSE;
+		isExit = FALSE;
+	
+	}
+
+	string Client::returnRsltHtml() {
+		string retrunStr = resultHtml;
+		resultHtml = "";
+		return retrunStr;
+	}
+
+	void Client::reciveFromServ() {
+		char buff[4096];
+		memset(buff, 0, sizeof(char) * 4096);
+
+		if (recv(sockfd, buff, 4096, 0) > 0) {
+			string recvStr(buff);
+			// 从server 取到输出结果，遇到 % 证明，可以向server 发送信息了
+			if (recvStr.find('%') != string::npos) isSend = true;
+
+			stringstream ss(recvStr);
+			string tempStr;
+
+			// 以换行为分隔符
+			while (getline(ss, tempStr, '\n')) {
+				//tempStr = substitute(tempStr, "\"", "&quot");
+				tempStr = substitute(tempStr, ">", "&gt");
+				tempStr = substitute(tempStr, "<", "&lt");
+				tempStr = substitute(tempStr, "%", "");
+				tempStr = substitute(tempStr, "\r", "");
+				tempStr = substitute(tempStr, "\n", "");
+
+				if (tempStr != " ") {
+					// 使用 javascript 打包好输出内容
+					saveToResult(tempStr);
+				}
+			}
+
+		}
+	}
+
+	void Client::sendToServ() {
+		string cmd = "";
+
+		ifstream batchFile(batFile.c_str());
+
+		batchFile.seekg(m_lastFilePosition);
+		if (getline(batchFile, cmd)) {
+			m_lastFilePosition += cmd.size() + 1;
+
+			for (int i = cmd.size() - 1; i > 0; i--)
+				if (cmd[i] == 13) cmd.pop_back();
+
+			// 粗体打包好从batFile读取的指令
+			string wrapCmd = "% ";
+			wrapCmd += "<b>";
+			wrapCmd += cmd;
+			wrapCmd += "</b>";
+			saveToResult(wrapCmd);
+			// 终端查看读取的文件信息
+			cerr << taskId << " " << cmd << " " << cmd.size() << endl;
+			cmd += '\n';
+			// 将指令通过socket 发送到server
+			// 注意在，linux 环境中 send 要换成 write
+			send(sockfd, cmd.c_str(), cmd.size(), 0);
+
+			if (cmd.find("exit") != string::npos) isExit = FALSE;
+			else 
+				isExit = true;
+		}
+		else {
+			AllocConsole();
+			freopen("CONOUT$", "w", stderr);
+			cerr << "No such batfile: " << batFile << endl;
+		}
+	}
+};
+
+
+// 这个类封装了 ras 客户类 Client 与 userNum
+// 作为浏览器与 ras 的衔接部分
+// 映射起一个浏览器请求与多个 ras 
+class singleClient{
+public:
+	singleClient(){
+		currentUserNum = 0;
+		serverSocketFD = 0;
+	}
+	~singleClient(){;}
+
+	void init(SOCKET server){
+		serverSocketFD = server;
+	}
+
+	Client* getANewJob(){
+		int lastUserNum = currentUserNum;
+		currentUserNum ++ ;
+		return &aClientJobs[lastUserNum];
+	}
+
+	Client* getAnExisJob(int i){
+
+		return &aClientJobs[i];
+	}
+
+	int getUserNum(){
+		return currentUserNum;
+	}
+
+	// 作为浏览器的 server socket
+	SOCKET serverSocketFD;
+	int currentUserNum;
+	Client aClientJobs[5];
+	bool isActive;
+};
+
+
+// 这个类主要管理浏览器 singleClient vector
+class clientHandler{
+public:
+
+	void addNewClient(SOCKET serverfd){
+		singleClient sC;
+		sC.init(serverfd);
+		vClients.push_back(sC);
+	}
+
+	void deleteClient(SOCKET serverfd){
+
+		int IDtoErase = -1;
+		for(int i = 0; i < vClients.size(); i++){
+			if(vClients[i].serverSocketFD == serverfd){
+				IDtoErase = i;
+				break;
+			}
+		}
+
+		if(IDtoErase == -1);
+		else{
+			vClients.erase(vClients.begin() + IDtoErase);
+		}
+	}
+
+	singleClient* findClient(SOCKET serverfd){
+		for(int i = 0; i < vClients.size(); i++){
+			if(vClients[i].serverSocketFD == serverfd)
+				return &vClients[i];
+		}
+		return NULL;
+	}
+
+	bool isClientExist(SOCKET serverfd){
+		for(int i = 0; i < vClients.size(); i++){
+			if(vClients[i].serverSocketFD == serverfd)
+				return true;
+		}
+		return false;
+	}
+	vector<singleClient> vClients;
+};
